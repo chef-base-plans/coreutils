4d6cdf574c19274092875ae07415a52c5eade2e0
Stuart Paterson <spaterson@chef.io>
2020-06-08 14:28:09 +0100

    Add coreutils.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# base-plan-skeleton
-Template for all new Chef Base Plans to simplify creation of repositories.
+[![Build Status](https://dev.azure.com/chefcorp-partnerengineering/Chef%20Base%20Plans/_apis/build/status/chef-base-plans.coreutils?branchName=master)](https://dev.azure.com/chefcorp-partnerengineering/Chef%20Base%20Plans/_build/latest?definitionId=145&branchName=master)
+
+# coreutils
+
+Basic file, shell and text manipulation utilities of the GNU operating system.
+
+## Maintainers
+
+* The Habitat Maintainers: <humans@habitat.sh>
+
+## Type of Package
+
+Binary package
+
+## Usage
+
+*TODO: Add instructions for usage*
---
+++ controls/coreutils_test.rb
@@ -0,0 +1,57 @@
+title 'Tests to confirm coreutils works as coreutilsed'
+
+plan_name = input('plan_name', value: 'coreutils')
+plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
+hab_path = input('hab_path', value: 'hab')
+
+control 'core-plans-coreutils' do
+  impact 1.0
+  title 'Ensure coreutils works'
+  desc '
+  To test coreutils is working as expected, we choose a subset of executables that ship with the
+  package and perform a few basic operations.  For example:
+
+    $ whoami
+    root
+
+    $ ls -al <path to ls>/ls
+    -rwxr-xr-x 1 root root 159272 Mar  5 23:19 <path to ls>/ls
+
+    $ env
+    <env listing>
+
+    $ echo "hello world"
+    hello world
+  '
+  coreutils_pkg_ident = command("#{hab_path} pkg path #{plan_ident}")
+  describe coreutils_pkg_ident do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+  coreutils_pkg_ident = coreutils_pkg_ident.stdout.strip
+
+  describe command("ls -al #{coreutils_pkg_ident}/bin/ls") do
+    its('stdout') { should_not be_empty }
+    its('stdout') { should match /#{coreutils_pkg_ident}/}
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("#{coreutils_pkg_ident}/bin/whoami") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+    its('stdout') { should match /root/ }
+  end
+
+  describe command("#{coreutils_pkg_ident}/bin/env") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+    its('stdout') { should match /PWD/ }
+    its('stdout') { should match /PATH/ }
+  end
+
+  describe command("#{coreutils_pkg_ident}/bin/echo 'hello world'") do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+    its('stdout') { should match /hello\s+world/ }
+  end
+end
